ae72d80c9e569bd197fd3684132665caa64afb26
dilaouid <dilaouid@student.42.fr>
2022-05-18 00:26:37 +0200

    (+) create subdirectory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeBtnComponent } from './home-btn/home-btn.component';
-import { HomeHeadingComponent } from './home-heading/home-heading.component';
+import { HomeBtnComponent } from './home/home-btn/home-btn.component';
+import { HomeHeadingComponent } from './home/home-heading/home-heading.component';
 
 @NgModule({
   declarations: [
--- src/app/models/home-btn.model.ts
+++
@@ -1,7 +0,0 @@
-export class HomeBtn {
-    constructor(public title: string,
-                public color: string,
-                public target: string) {
-
-    }
-};
