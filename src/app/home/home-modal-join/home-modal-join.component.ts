6cbf074e91793ba17db83b2fca3daa2fd5b11bc3
dilaouid <dilaouid@student.42.fr>
2022-05-18 22:34:41 +0200

    (+) input validation border joining modal

--- src/app/home/home-modal-join/home-modal-join.component.ts
+++ src/app/home/home-modal-join/home-modal-join.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InputValidation } from '../models/home-modal-validation';
 
 @Component({
   selector: 'app-home-modal-join',
@@ -10,34 +11,56 @@ export class HomeModalJoinComponent {
   enabledButton: boolean;
   displayAlert: boolean;
   
-  roomID!: string;
-  username!: string;
-  password!: string;
+  username!: InputValidation;
+  password!: InputValidation;
+  roomID!: InputValidation;
 
   constructor() {
     this.enabledButton = false;
     this.displayAlert = false;
+    this.username = new InputValidation('', false, false);
+    this.password = new InputValidation('', false, false);
+    this.roomID = new InputValidation('', false, false);
   }
 
   checkLength() {
-    console.log(this.roomID);
-    
-    return this.username?.length > 2 && this.password?.length > 2 && this.roomID?.length >= 18;
+    return this.username?.value?.length > 2 && this.password?.value?.length > 2 && this.roomID?.value?.length === 18;
   }
 
   changeUsername(username: any) {
-    this.username = username?.value?.trim();
+    this.username.value = username?.value?.trim();
     this.enabledButton = this.checkLength();
+    if (this.username.value.length <= 2) {
+      this.username.wrong = this.username.value?.length > 0 ? true : false;
+      this.username.valid = false;
+    } else {
+      this.username.valid = true;
+      this.username.wrong = false;
+    }
   }
 
   changeRoom(room: any) {
-    this.roomID = room?.value?.trim();
+    this.roomID.value = room?.value?.trim();
     this.enabledButton = this.checkLength();
+    if (this.roomID.value.length < 18) {
+      this.roomID.wrong = this.roomID.value?.length > 0 ? true : false;;
+      this.roomID.valid = false;
+    } else {
+      this.roomID.valid = true;
+      this.roomID.wrong = false;
+    }
   }
 
   changePassword(password: any) {
-    this.password = password?.value?.trim();
+    this.password.value = password?.value?.trim();
     this.enabledButton = this.checkLength();
+    if (this.password.value.length <= 2) {
+      this.password.wrong = this.password.value?.length > 0 ? true : false;;
+      this.password.valid = false;
+    } else {
+      this.password.valid = true;
+      this.password.wrong = false;
+    }
   }
 
   submit() {
