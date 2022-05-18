024a0fdcbcebc7b23c34af80abea33f4f5711a3f
dilaouid <dilaouid@student.42.fr>
2022-05-18 18:12:58 +0200

    (+) first model interaction homepage complete

--- src/app/home/home-modal-create/home-modal-create.component.ts
+++ src/app/home/home-modal-create/home-modal-create.component.ts
@@ -1,25 +1,42 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-modal-create',
   templateUrl: './home-modal-create.component.html',
   styleUrls: ['./home-modal-create.component.css']
 })
-export class HomeModalCreateComponent implements OnInit {
+export class HomeModalCreateComponent {
 
-  disabledButton: boolean;
+  enabledButton: boolean;
   displayAlert: boolean;
   
   username!: string;
   password!: string;
 
+
   constructor() {
-    this.disabledButton = true;
+    this.enabledButton = false;
     this.displayAlert = false;
   }
 
-  ngOnInit(): void {
-    
+  checkLength() {
+    return this.username?.length > 2 && this.password?.length > 2;
+  }
+
+  changeUsername(username: any) {
+    this.username = username?.value?.trim();
+    this.enabledButton = this.checkLength();
+  }
+
+  changePassword(password: any) {
+    this.password = password?.value?.trim();
+    this.enabledButton = this.checkLength();
+  }
+
+  submit() {
+    if ( !this.checkLength() ) {
+      this.displayAlert = true;
+    } else this.displayAlert = false;
   }
 
 }
