120faca30e2413b034ba7eaeab22fa180109f9e5
dilaouid <dilaouid@student.42.fr>
2022-05-18 18:35:20 +0200

    (+) second modal interaction completed

--- src/app/home/home-modal-join/home-modal-join.component.ts
+++ src/app/home/home-modal-join/home-modal-join.component.ts
@@ -1,24 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-modal-join',
   templateUrl: './home-modal-join.component.html',
   styleUrls: ['./home-modal-join.component.css']
 })
-export class HomeModalJoinComponent implements OnInit {
+export class HomeModalJoinComponent {
 
-  disabledButton: boolean;
+  enabledButton: boolean;
   displayAlert: boolean;
   
   roomID!: string;
+  username!: string;
   password!: string;
 
   constructor() {
-    this.disabledButton = true;
+    this.enabledButton = false;
     this.displayAlert = false;
   }
 
-  ngOnInit(): void {
+  checkLength() {
+    console.log(this.roomID);
+    
+    return this.username?.length > 2 && this.password?.length > 2 && this.roomID?.length >= 18;
   }
+
+  changeUsername(username: any) {
+    this.username = username?.value?.trim();
+    this.enabledButton = this.checkLength();
+  }
+
+  changeRoom(room: any) {
+    this.roomID = room?.value?.trim();
+    this.enabledButton = this.checkLength();
+  }
+
+  changePassword(password: any) {
+    this.password = password?.value?.trim();
+    this.enabledButton = this.checkLength();
+  }
+
+  submit() {
+    if ( !this.checkLength() ) {
+      this.displayAlert = true;
+    } else this.displayAlert = false;
+  }
+
 
 }
