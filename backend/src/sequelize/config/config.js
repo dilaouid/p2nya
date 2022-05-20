75d1eea5ece1e2e4fc47dcddd3fe6ffff1f0e413
dilaouid <dilaouid@student.42.fr>
2022-05-20 22:23:02 +0200

    (+) init sequelize

---
+++ backend/src/sequelize/config/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+    "development" : {
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.DB_HOST,
+        "dialect": process.env.DB_DIALECT,
+        "port": process.env.DB_PORT
+    }
+};
