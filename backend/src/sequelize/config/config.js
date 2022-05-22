0c987ff21834c63c49981a667e1b683ab85230d4
dilaouid <dilaouid@student.42.fr>
2022-05-23 00:06:47 +0200

    (+) take out sequelize logging

--- backend/src/sequelize/config/config.js
+++ backend/src/sequelize/config/config.js
@@ -7,6 +7,7 @@ module.exports = {
         "database": process.env.DB_NAME,
         "host": process.env.DB_HOST,
         "dialect": process.env.DB_DIALECT,
-        "port": process.env.DB_PORT
+        "port": process.env.DB_PORT,
+        "logging": false
     }
 };
