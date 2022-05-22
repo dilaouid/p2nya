bc2f84e5483d6ae74e06786028349b286b142c88
dilaouid <dilaouid@student.42.fr>
2022-05-22 23:58:54 +0200

    (+) seeders users [random]

---
+++ backend/src/sequelize/seeders/users.ts
@@ -0,0 +1,14 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const users = [
+    {
+        uuid: uuidv4(),
+        username: 'yedine',
+        ip: '90.412.12.25'
+    },
+    {
+        uuid: uuidv4(),
+        username: 'kiloutre',
+        ip: '89.25.4.77'
+    }
+];
