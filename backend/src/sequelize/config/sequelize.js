f37247d2b7dd6aa4409de055ed7723f8df02bf15
dilaouid <dilaouid@student.42.fr>
2022-06-03 00:15:36 +0200

    (~) config change sequelize format

--- backend/src/sequelize/config/sequelize.js
+++ backend/src/sequelize/config/sequelize.js
@@ -1,34 +1,33 @@
-import dotenv from 'dotenv'
-dotenv.config()
+require('dotenv').config();
 
-export default {
-    development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
-        dialectOptions: {
+module.exports = {
+    "development": {
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
+        "dialect": process.env.DB_DIALECT,
+        "dialectOptions": {
             bigNumberStrings: true
         }
     },
-    test: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
+    "test": {
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
+        "dialect": process.env.DB_DIALECT,
     },
-    production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
-        dialectOptions: {
+    "production": {
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
+        "dialect": process.env.DB_DIALECT,
+        "dialectOptions": {
             bigNumberStrings: true,
         }
     }
