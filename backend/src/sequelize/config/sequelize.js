63f7a22e220ffd0f30358237bde2d783ded4b1b3
dilaouid <dilaouid@student.42.fr>
2022-06-02 23:51:12 +0200

    (~) replace config.js by sequelize.js (add gitignore for explicit datas)

---
+++ backend/src/sequelize/config/sequelize.js
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export default {
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+        dialectOptions: {
+            bigNumberStrings: true
+        }
+    },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+        dialectOptions: {
+            bigNumberStrings: true,
+        }
+    }
+};
