1beb8fca018e7e47aea774dd92c565e6f4d8ad0c
dilaouid <dilaouid@student.42.fr>
2022-06-03 19:37:14 +0200

    (~) update routes return types

--- backend/src/API/rooms/rooms.api.ts
+++ backend/src/API/rooms/rooms.api.ts
@@ -5,7 +5,7 @@ import db from "../../Sequelize/models";
 const rooms = express();
 
 /* Count all the active rooms ( where attributes to changes [todo] ) */
-rooms.get('/count', async (req: Request, res: Response) => {
+rooms.get('/count', async (req: Request, res: Response): Promise<Response> => {
     return send(200, 'OK', [await db.Room.count({})], res);
 });
 
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,20 +18,15 @@ app.use(e({ credentials: true, origin: ['http://localhost:4200'] }));
 app.use(mw());
 
 /** (GET) [ FIRST CALL ] >> Once the user comes to the homepage, this call is made -- Used to read or to create a new token for the user */
-app.get('/', isAuthentified, async (req: Request, res: Response): Promise<void> => {
+app.get('/', isAuthentified, (req: Request, res: Response): Response => {
   return send(200, 'OK', [], res)
 });
 
 app.use('/api', api);
 
 /** [ [[ 404 ]] ] */
-app.get('*', (req: Request, res: Response): void => {
-  res.status(404).send({
-      statut: 404,
-      res: 'Not Found',
-      success: false,
-      data: []
-  });
+app.get('*', (req: Request, res: Response): Response => {
+  return send(404, 'Not Found', [], res); 
 });
 
 /** [ LISTENING ON PORT ] */
