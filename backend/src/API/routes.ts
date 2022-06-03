2794e78e6b259acac5169f005c48f5d8bef0a13e
dilaouid <dilaouid@student.42.fr>
2022-06-03 16:46:53 +0200

    (+) fix router name

--- backend/src/API/routes.ts
+++ backend/src/API/routes.ts
@@ -1,8 +1,8 @@
-import router from './rooms/rooms.api';
+import rooms from './rooms/rooms.api';
 
 const express         = require('express');
 const api          = express.Router();
 
-api.use('/rooms', router);
+api.use('/rooms', rooms);
 
 export default api;
