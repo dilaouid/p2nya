0bed9c767d1eb374bcd85d11f564c0e1ced974e3
dilaouid <dilaouid@student.42.fr>
2022-06-09 00:36:17 +0200

    (+) create config.template file for Sequelize

---
+++ backend/src/sequelize/config/config.template.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = {
+    "development" : {
+        "username": '',
+        "password": '',
+        "database": '',
+        "host": 'localhost',
+        "dialect": 'mysql',
+        "port": 3306,
+        "logging": false
+    }
+};
