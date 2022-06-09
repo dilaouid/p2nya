bfc32108ceb3cc433f8195caa449bbe9f10647a5
dilaouid <dilaouid@student.42.fr>
2022-06-10 00:35:03 +0200

    (+) create route to read user profile picture and set a default one

--- backend/src/API/users/users.api.ts
+++ backend/src/API/users/users.api.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { v4 as uuidv4 } from 'uuid';
 import { send } from "../../utils/response";
+import * as fs from 'fs-extra';
 import { getUserID, isAuthentified } from "../mw";
 import db from "../../Sequelize/models";
 
@@ -10,10 +11,32 @@ interface UpdateUser {
     picture: string;
 };
 
+/* Return the profile picture of a specific user */
+users.get('/picture/:uuid', isAuthentified, (req: Request, res: Response): void => {
+    const uuid: string = req.params.uuid || null;
+    const allowedExtension: string[] = ['png', 'jpg', 'jpeg', 'gif'];
+
+    let i: number = 0;
+    let fileExists: boolean = false;
+    let file: string;
+    while (i < allowedExtension.length && fileExists === false) {
+        let path: string = `./src/uploads/profile/${uuid}/picture.${allowedExtension[i]}`;
+        if (fs.existsSync(path) === true) {
+            fileExists = true;
+            file = fs.readFileSync(path);
+        }
+        i++;
+    }
+    if (fileExists === false)
+        file = fs.readFileSync(`./src/uploads/profile/default.jpg`);
+    res.contentType(fileExists ? `image/${allowedExtension[i]}` : 'image/jpg');
+    res.send(file);
+});
+
 /* Update the logged user information (username and profil picture) */
 users.put('/', isAuthentified, async (req: Request, res: Response):Promise<Response> => {
     const update: UpdateUser = req.body;
-    const userId = await getUserID(req.cookies.token);
+    const userId: number = await getUserID(req.cookies.token);
     update.username = update?.username?.trim() || null;
     if (update.username) {
         if (update.username.length < 2 || update.username.length > 20) {
