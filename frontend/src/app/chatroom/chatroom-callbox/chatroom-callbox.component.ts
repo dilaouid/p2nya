511665ff86e612c95954218de43677189046f384
dilaouid <dilaouid@student.42.fr>
2022-06-10 23:51:34 +0200

    (+) init callbox component

--- frontend/src/app/chatroom/chatroom-callbox/chatroom-callbox.component.ts
+++ frontend/src/app/chatroom/chatroom-callbox/chatroom-callbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-chatroom-callbox',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatroomCallboxComponent implements OnInit {
 
+  @Input() inCall: any[] | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
