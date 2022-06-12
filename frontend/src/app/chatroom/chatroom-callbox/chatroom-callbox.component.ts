af773f5c7b970763fc84154fa01f700d8f4503f2
dilaouid <dilaouid@student.42.fr>
2022-06-12 20:30:32 +0200

    (+) not-yet modal inside ng-template

--- frontend/src/app/chatroom/chatroom-callbox/chatroom-callbox.component.ts
+++ frontend/src/app/chatroom/chatroom-callbox/chatroom-callbox.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-chatroom-callbox',
@@ -10,13 +11,31 @@ export class ChatroomCallboxComponent implements OnInit {
 
   @Input() inCall: any[] | undefined;
   @Input() userInCall: boolean | undefined;
+  closeResult!: string;
   api: string;
 
-  constructor() {
+  constructor(private modalService: NgbModal) {
     this.api = environment.api;
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
+
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', centered: true}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  };
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  };
 
 }
