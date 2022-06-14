589a9a4dca32473473458f435ada475ceab54265
dilaouid <dilaouid@student.42.fr>
2022-06-15 00:53:26 +0200

    (~) fix issue with multiple profile picture live update

--- frontend/src/app/utils/helpers.ts
+++ frontend/src/app/utils/helpers.ts
@@ -2,6 +2,8 @@ import { ElementRef, QueryList } from "@angular/core";
 
 export const updateProfilPictureLive = (uuid: any, list: QueryList<ElementRef>) => {
     let nd = new Date().getTime();
-    var correctDOM: ElementRef<any> = list.filter( t => t.nativeElement.attributes.user.value === uuid)[0];
-    correctDOM.nativeElement.src = correctDOM.nativeElement.src + '?t=' + nd;
+    var correctDOM: ElementRef[] = list.filter( t => t.nativeElement.attributes.user.value === uuid);
+    for (let o = 0; o < correctDOM.length; o++) {
+        correctDOM[o].nativeElement.src = correctDOM[o].nativeElement.src + '?t=' + nd;
+    }
 };
