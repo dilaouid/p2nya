cb296794f640a36d3ec23d8a6addafe76d3f1f70
dilaouid <dilaouid@student.42.fr>
2022-06-16 00:52:26 +0200

    (-) delete chatroom modal js

--- frontend/src/assets/js/chatroom_modal.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-    $('#usernameEdit').on('keydown paste', function(event) {
-        if($(this).text().length === 17 && event.keyCode != 8) { 
-          event.preventDefault();
-        }
-    });
-});
