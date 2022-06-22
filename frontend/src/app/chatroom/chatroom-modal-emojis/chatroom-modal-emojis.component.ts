92c07353117a2ed929c0d42c8ff485c078278394
dilaouid <dilaouid@student.42.fr>
2022-06-23 00:40:36 +0200

    (+) improve interaction through emoji modal

--- frontend/src/app/chatroom/chatroom-modal-emojis/chatroom-modal-emojis.component.ts
+++ frontend/src/app/chatroom/chatroom-modal-emojis/chatroom-modal-emojis.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+interface Emoji {
+  alias: string;
+  base64: string;
+}
+
 @Component({
   selector: 'app-chatroom-modal-emojis',
   templateUrl: './chatroom-modal-emojis.component.html',
@@ -8,13 +13,26 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ChatroomModalEmojisComponent implements OnInit {
 
+  emojis!: Emoji[];
+  displayAlert: boolean = false;
+
   constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+    const lSEmojis = localStorage.getItem('emoji');
+    if (lSEmojis) this.emojis = JSON.parse(lSEmojis);
   }
 
   closeModal() {
     this.modalService.dismissAll();
+  }
+
+  removeEmoji(index: number) {
+     this.emojis.splice(index, 1);
+  }
+
+  addEmoji() {
+    this.emojis.push({alias: '', base64: './assets/img/emojis/template.webp'});
   }
 
   
